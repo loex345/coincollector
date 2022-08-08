31cf390067fe070a1d557bd2c292894361d470ca
loex345 <maitlandeg@gmail.com>
2022-08-08 11:55:37 -0700

    coin database

--- main_app/views.py
+++ main_app/views.py
@@ -3,17 +3,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Add the Cat class & list and view function below the imports
-class Coin:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, name, type, description, value):
-    self.name = name
-    self.type = type
-    self.description = description
-    self.value = value
-
-coins = [
-  Coin('Peace Dollar', 'silver', 'peace dollar', 1),
-  Coin('Lincoln Cent', 'bronze', 'Lincoln coin', .01),
-]
 
 
 # Create your views here.
